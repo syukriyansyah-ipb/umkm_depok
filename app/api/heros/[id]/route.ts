d19000e74619a3186fd58827e2376e2e1dc3b2a8
syukriyansyah-ipb <2020syukriyansyah@apps.ipb.ac.id>
2025-02-07 07:19:49 +0700

    fixed

--- app/api/heros/[id]/route.ts
+++ app/api/heros/[id]/route.ts
@@ -1,34 +1,33 @@
-import  dbConnect  from "@/lib/db";
+import dbConnect from "@/lib/db";
 import Hero from "@/libs/models/Hero";
 import { NextRequest, NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
 
-export async function PUT(request: NextRequest, context: { params: { id: string } }) {
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
   try {
-    const { params } = context;
-    const id = params.id;
-    const body = await request.json();
-    const { title, description, image, backgroundImage, imageKey, backgroundImageKey, tiktok, instagram, shopee, tokopedia } = body;
-
     await dbConnect();
-    const updatedHero = await Hero.findByIdAndUpdate(new ObjectId(id), { title, description, image, backgroundImage, imageKey, backgroundImageKey, tiktok, instagram, shopee, tokopedia });
+    const body = await request.json();
+    const { id } = params; // Ambil ID dari params
+
+    const updatedHero = await Hero.findByIdAndUpdate(new ObjectId(id), body, { new: true });
 
     return NextResponse.json({ message: "Hero updated successfully", data: updatedHero });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return NextResponse.json({ message: "Failed to update hero", error }, { status: 400 });
   }
 }
 
 export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
   try {
-    const id = params.id;
-
     await dbConnect();
-    await Hero.findByIdAndDelete(new ObjectId(id) );
+    const { id } = params; // Ambil ID dari params
+
+    await Hero.findByIdAndDelete(new ObjectId(id));
 
     return NextResponse.json({ message: "Hero deleted successfully" });
   } catch (error) {
+    console.error(error);
     return NextResponse.json({ message: "Failed to delete hero", error }, { status: 400 });
   }
 }
