6bec1cc41e0689db14bc752857493f9a41328c12
syukriyansyah-ipb <2020syukriyansyah@apps.ipb.ac.id>
2025-02-19 09:35:09 +0700

    add speedAnalytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google"
 import { useState, useEffect } from "react"
 import { Providers } from "@/app/providers"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -39,6 +40,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <Analytics />
+        <SpeedInsights />
         <Providers>{children}</Providers>
       </body>
     </html>
